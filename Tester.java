84f667a74d02fbc0d3c1affbbea910854dd914b0
JesseStuy20 <jxie20@stuy.edu>
2020-10-23 13:22:20 -0400

    Added the sum method and test cases

---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+  public static void main(String[] args) {
+    int[] a = {1,6,3,2,6} ;
+    System.out.println(ArrayOps.sum(a)) ;
+    int[] b = {12,8,61,8,3,26} ;
+    System.out.println(ArrayOps.sum(b)) ;
+    int[] c = {73,1,7,5,2,8,3,100} ;
+    System.out.println(ArrayOps.sum(c)) ;
+  }
+}
